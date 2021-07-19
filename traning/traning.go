04e69d7090af6282eb43543e7302b2210a9bd6bc
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-07-20 00:36:43 +0900

    練習問題2

--- traning/traning.go
+++ traning/traning.go
@@ -1,6 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+func minInt(a []int) int {
+	sort.Ints(sort.IntSlice(a))
+	return a[0]
+}
+
+func sum(s map[string]int) (n int) {
+	for _, t := range s {
+		n += t
+	}
+	return
+}
 
 func main() {
 	f1 := 1.11
@@ -9,4 +24,17 @@ func main() {
 
 	m := map[string]int{"Mike": 20, "Nancy": 24, "Messi": 30}
 	fmt.Printf("%T %v", m, m)
+
+	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
+	fmt.Println(minInt(l))
+
+	s := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+	fmt.Println(sum(s))
 }
